7fd9ac2b24fbccd8ce7a3721fa0ed9e284e45972
raymanoz <rbarlow@raymanoz.com>
2011-08-17 22:58:03 +0100

    Or not :(

--- src/test/scala/HangSpec.scala
+++ src/test/scala/HangSpec.scala
@@ -1,28 +1,15 @@
 import code.MyServer
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.specs2.execute.Result
+import org.openqa.selenium.firefox.{FirefoxProfile, FirefoxDriver}
 import org.specs2.mutable.Specification
-import org.specs2.specification.Around
 
 class HangSpec extends Specification {
-  "this test completes, but jvm never shutsdown. But using an Around trait fixed it!" in new Webserver {
-    val webDriver = new FirefoxDriver()
+  "this test completes, but jvm never shutsdown" in {
+    val server = new MyServer().start()
+    val webDriver = new FirefoxDriver(new FirefoxProfile())
     webDriver.navigate.to("http://localhost:8080/")
     Thread.sleep(1000)
     webDriver.close()
+    server.stop()
     success
-  }
-}
-
-
-trait Webserver extends Around {
-  lazy val server = new MyServer()
-  def around[T <% Result](t: =>T) = {
-    server.start()
-    try {
-      t
-    } finally {
-      server.stop()
-    }
   }
 }
